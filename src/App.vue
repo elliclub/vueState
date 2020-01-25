02f954bd5af5a46e9585de1a65d92bf706b638ab
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-01-25 17:44:14 +0100

    add som styling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+a:link {
+  text-decoration: none;
+  color: #6d6875;
+  font-size: 24px;
+}
+a:hover {
+  text-decoration: underline;
+  color: #b5838d;
+}
 </style>
