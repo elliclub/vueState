c312ff197aa9c15b52a5a94076ef37d19b904683
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-10-16 22:21:44 +0200

    add randomize method

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    One user
-    <div v-if="user">{{user[0].id}}</div>
+    One user:
+    <span v-if="user">{{user[0].id}}</span>
+    <div>
+      <button @click="randomize()">Randomize!</button>
+      <div>Randomized user: {{randomizedUser}}</div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,19 +13,24 @@ export default {
   name: "requestUsers",
   data() {
     return {
-      user: ""
+      user: "",
+      randomizedUser: ""
     };
   },
   mounted() {
     this.fetchData();
   },
+  computed: {},
   methods: {
     async fetchData() {
       let url = "https://reqres.in/api/users?page=2";
       const response = await fetch(url);
       const data = await response.json();
       this.user = data.data;
-      console.log(this.user[0].id);
+    },
+    randomize() {
+      const random = Math.floor(Math.random() * this.user.length);
+      return (this.randomizedUser = this.user[random].first_name);
     }
   }
 };
